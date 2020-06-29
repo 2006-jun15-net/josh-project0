1385d501ddf1458899c1d533fd74c49f4ac6758b
Josh Bertrand <joshuabertrand@protonmail.com>
2020-06-28 20:24:57 -0400

    Began development of UI

--- Project0/ConsoleApp/Program.cs
+++ Project0/ConsoleApp/Program.cs
@@ -11,14 +11,20 @@ namespace Project0
 
             WelcomeMessage();
 
-            initializeProgram();
+            List<Product> products = new List<Product>();
+            List<Customer> customers = new List<Customer>();
 
-            /*
             Product product1 = new Product("Pencils", 0.75);
             Product product2 = new Product("Eraser", 0.50);
             Product product3 = new Product("Pencil Case", 1.50);
 
+            products.Add(product1);
+            products.Add(product2);
+            products.Add(product3);
+
             Customer cust1 = new Customer("Josh", "Bertrand");
+
+            customers.Add(cust1);
 
             Dictionary<Product, int> sampleProductInv = new Dictionary<Product, int>();
             sampleProductInv.Add(product1, 2);
@@ -26,23 +32,25 @@ namespace Project0
 
             StoreLocation store1 = new StoreLocation("Bookstore", "123 Here Street", sampleProductInv);
 
-            List<Product> orderList = new List<Product>();
-            orderList.Add(product1);
-            orderList.Add(product2);
+            Order newOrder = new Order(store1.StoreId, cust1.customerId);
 
-            Order firstOrder = new Order(store1.storeName, cust1, orderList);
+            Order firstOrder = new Order(store1.StoreId, cust1.customerId);
 
-            firstOrder.checkout(store1, cust1, firstOrder);
-            */
+            bool isSuccessful;
+            isSuccessful = firstOrder.Checkout(store1, cust1);
+
+            //place order
+            //add customer
+            //search customers
+            //display order details
+            //display order history of store
+            //display order history of customer
+            
         }
 
         public static void WelcomeMessage()
         {
             Console.WriteLine("\nWelcome to our Store!\n");
-        }
-        public static void initializeProgram()
-        {
-
         }
 
         //add new customer
